2afe2391781f436bdc21517d2c9f93a7d49cc4a2
study80s <study80s@gmail.com>
2024-07-08 17:21:04 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './test.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <React.StrictMode>
+          <test />
+        </React.StrictMode>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <test/>
         </p>
         <a
           className="App-link"
---
+++ src/test.js
@@ -0,0 +1,9 @@
+function test(){
+    return (
+        <div>
+            오늘은 리액트 첫 수업날 입니다.
+        </div>
+    )
+}
+
+export default test;
